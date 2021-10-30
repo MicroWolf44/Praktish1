33f695a31bc9c00ab5be1b43c3b786a35b84f012
MicroWolf44 <microwolf44@gmail.com>
2021-10-31 01:02:16 +0300

    git branch

--- src/package-info.java
+++ src/package-info.java
@@ -10,5 +10,12 @@
  *  - poisk zapisi po klyuchu;
  *  - dobavlenie zapisei pri uslovii sootvetstviya trebovaniyam konkretnogo slovarya;
  * 2 - Dolzhno bit' predostavleno konsol'noe prilojenie, rabotayuzhee s servisami slovarya:
- *  -
+ *  - obespechit' vozmojnost' raboti so slovarem dvih yazikov:
+ *      v pervom yazike dlinna slov mojet bit' tol'ko 4 simvola i eti simvoli tol'ko bukvi latinskoi raskladki
+ *      vtoroi yazik - dlinna slov mojet bit' tolko 5 simvolov i eti simvoli tol'ko cifri.
+ *  - interfeis dolzhen predostavlyat' sleduyuzhuyu funkcionalnost' pol'zovatelyu:
+ *      - prosmotr soderjimogo oboih slovarei;
+ *      - udalenie;
+ *      - poisk po kluchu;
+ *      - dobavlenie zapisi v odnom iz slovarei, zaranee vibrannom polzovatelem;
  */
